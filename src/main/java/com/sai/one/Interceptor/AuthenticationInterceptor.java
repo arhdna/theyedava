b3bd5e91409c17cf5927f7d4c02d4bf8ea74e4a0
arhdna <white8048@gmail.com>
2016-10-09 10:18:50 +0530

    commented

--- src/main/java/com/sai/one/Interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/sai/one/Interceptor/AuthenticationInterceptor.java
@@ -18,31 +18,31 @@ public class AuthenticationInterceptor  extends HandlerInterceptorAdapter {
     @Autowired
     private EncryptDecryptUtil encryptDecryptUtil;
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String authToken = request.getHeader(MobileServiceConstants.X_AKOSHA_ACCESS_TOKEN);
-        if (!StringUtils.isBlank(akoshaAccessCode)) {
-
-            try {
-                V3UserAuth userAuth = tokenHandler.parseV3UserFromToken(akoshaAccessCode);
-                LOGGER.debug("V3 user auth mobile: " + userAuth.getMobile() + " & id: " + userAuth.getId());
-
-                if (userAuth != null && userAuth.getId() > 0) {
-                    String authToken = getAccessCodes(userAuth.getId());
-                    if (authToken.equals(akoshaAccessCode)) {
-                        LOGGER.info("PING:::" + userAuth.getId() + ":::PING:::INTERCEPTOR");
-                        //LOGGER.debug("Authentication successful!");
-                        request.setAttribute(MobileServiceConstants.USER_AUTH, userAuth);
-                        return true;
-                    }
-                }
-            } catch (Exception e) {
-                LOGGER.error("", e);
-            }
-        }
-
-        LOGGER.info("401 unauthorized for token: " + akoshaAccessCode);
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        return false;
-    }
+   // @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        String authToken = request.getHeader(MobileServiceConstants.X_AKOSHA_ACCESS_TOKEN);
+//        if (!StringUtils.isBlank(akoshaAccessCode)) {
+//
+//            try {
+//                V3UserAuth userAuth = tokenHandler.parseV3UserFromToken(akoshaAccessCode);
+//                LOGGER.debug("V3 user auth mobile: " + userAuth.getMobile() + " & id: " + userAuth.getId());
+//
+//                if (userAuth != null && userAuth.getId() > 0) {
+//                    String authToken = getAccessCodes(userAuth.getId());
+//                    if (authToken.equals(akoshaAccessCode)) {
+//                        LOGGER.info("PING:::" + userAuth.getId() + ":::PING:::INTERCEPTOR");
+//                        //LOGGER.debug("Authentication successful!");
+//                        request.setAttribute(MobileServiceConstants.USER_AUTH, userAuth);
+//                        return true;
+//                    }
+//                }
+//            } catch (Exception e) {
+//                LOGGER.error("", e);
+//            }
+//        }
+//
+//        LOGGER.info("401 unauthorized for token: " + akoshaAccessCode);
+//        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+//        return false;
+//    }
 }
--- src/main/java/com/sai/one/chache/CouchBaseImpl.java
+++ src/main/java/com/sai/one/chache/CouchBaseImpl.java
@@ -6,40 +6,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CouchBaseImpl implements Cache {
+public class CouchBaseImpl  {
 
-    @Autowired
-    @Qualifier("createCouchCluster")
-    private Bucket bucket;
 
-    @Override
-    public void save(String key, Object value) {
-        bucket.insert(key,value);
-    }
 
-    @Override
-    public void update(String key, Object value) {
-        bucket.replace(key,value)
-    }
-
-    @Override
-    public void upsert(String key, Object value) {
-        bucket.upsert(key);
-    }
-
-    @Override
-    public void delete(String key) {
-        bucket.remove(key);
-    }
-
-    @Override
-    public void get(String key, boolean touch, short timer) {
-        if(touch)
-        {
-            bucket.getAndTouch(key,timer);
-        }else{
-            bucket.get(key);
-        }
-
-    }
 }
